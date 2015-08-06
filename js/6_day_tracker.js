f8ee7cbd8a2faa2bc808a6b501698635cff6f3bb
liuyuanswu <liuyuanswu@163.com>
2015-08-06 20:59:52 +0800

    上传第六天的项目

--- js/6_day_tracker.js
+++ js/6_day_tracker.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
-	var d=new Date().getDay();
-	var day=new Array(7);
-	day[0]="星期日";
-	day[1]="星期一";
-	day[2]="星期二";
-	day[3]="星期三";
-	day[4]="星期四";
-	day[5]="星期五";
-	day[6]="星期六";
-	if ($(".day".attr("id"))==(String)d) {
-		
-		$("#today_welcome").html("今天" + day[d]);
-	};
-	
+	var day=new Date().getDay();
+	var week=[
+			'星期日 伤day',
+			'星期一 忙day',
+			'星期二 求死day',
+			'星期三 未死day',
+			'星期四 受死day',
+			'星期五 福来day',
+			'星期六 洒脱day'
+			];
+	$("#" + day).attr("id","today");
+	$("#today_welcome").html(week[day]);
 });
